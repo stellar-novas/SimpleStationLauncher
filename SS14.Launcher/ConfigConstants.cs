d513850f2fc9a0adf2e596d7e157bd4801394394
DEATHB4DEFEAT <zachcaffee@outlook.com>
2024-12-30 13:37:05 -0800

    v1.1.1

--- SS14.Launcher/ConfigConstants.cs
+++ SS14.Launcher/ConfigConstants.cs
@@ -6,7 +6,7 @@ namespace SS14.Launcher;
 
 public static class ConfigConstants
 {
-    public const string CurrentLauncherVersion = "1.1.0";
+    public const string CurrentLauncherVersion = "1.1.1";
     #if RELEASE
     public const bool DoVersionCheck = true;
     #else
