54aec0558d6cdbd5b0a3952fe4836c28785949b6
Nova <stellar_novas@riseup.net>
2025-01-04 19:17:50 -0500

    bad qemu version

--- flake.nix
+++ flake.nix
@@ -1,48 +1,99 @@
 {
   description = "Flake providing a package for the SimpleStation14 Launcher.";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/release-24.05";
-  inputs.flake-compat = {
-    url = "github:edolstra/flake-compat";
-    flake = false;
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/release-24.11";
+  inputs.nixos-generators = {
+    url = "github:nix-community/nixos-generators";
+    inputs.nixpkgs.follows = "nixpkgs";
   };
 
-
-  outputs = { self, nixpkgs, ... }:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      nixos-generators,
+      ...
+    }:
     let
-      forAllSystems = function:
-        nixpkgs.lib.genAttrs [ "x86_64-linux" ] # TODO: aarch64-linux support
-          (system: function (import nixpkgs { inherit system; }));
+      forAllSystems =
+        function:
+        nixpkgs.lib.genAttrs [ "x86_64-linux" "aarch64-linux" ] (
+          system: function (import nixpkgs { inherit system; })
+        );
+      isousername = "ss14-developer";
     in
     {
       packages = forAllSystems (pkgs: {
         default = self.packages.${pkgs.system}.space-station-14-launcher;
-        space-station-14-launcher =
-          pkgs.callPackage ./nix/package.nix { };
+        space-station-14-launcher = pkgs.callPackage ./nix/package-git.nix { };
+
+        test-env-image = nixos-generators.nixosGenerate {
+          system = pkgs.system;
+          format = "iso";
+          modules = [
+            {
+              imports = [
+                "${nixpkgs}/nixos/modules/profiles/qemu-guest.nix"
+              ];
+
+              users.users.isouser = {
+                name = isousername;
+                isNormalUser = true;
+                createHome = true;
+              };
+
+              services.pipewire = {
+                enable = true;
+                alsa.enable = true;
+                alsa.support32Bit = true;
+                pulse.enable = true;
+              };
+
+              fileSystems."/xdg-data" = {
+                fsType = "9p";
+                label = "xdg-data";
+                options = [
+                  "trans=virtio"
+                ];
+              };
+
+              fileSystems."/app" = {
+                fsType = "9p";
+                label = "app-path";
+                options = [
+                  "trans=virtio"
+                ];
+              };
+
+              services.cage = {
+                enable = true;
+                user = isousername;
+                environment = {
+                  XDG_DATA_HOME = "/xdg-data";
+                };
+                program = "/app/bin/SS14.Launcher";
+              };
+
+              system.stateVersion = "24.11";
+            }
+          ];
+        };
+
+        test-x86-linux = pkgs.writeShellScriptBin "test-launcher-x86" ''
+          ${pkgs.qemu}/bin/qemu-system-x86_64 \
+            -cpu max \
+            -m 2G \
+            -smp 4 \
+            -drive file=${self.packages.x86_64-linux.test-env-image}/iso/nixos.iso,media=cdrom,readonly=on \
+            -virtfs local,path=$XDG_DATA_HOME,mount_tag=xdg-data,security_model=passthrough,id=xdg-data \
+            -virtfs local,path=${
+              self.packages.${pkgs.system}.default
+            },mount_tag=app-path,security_model=passthrough,id=app-path \
+            -vga qxl
+        '';
+
       });
 
-      overlays = {
-        default = self.overlays.space-station-14-launcher;
-        space-station-14-launcher = final: prev: {
-          space-station-14-launcher =
-            self.packages.${prev.system}.space-station-14-launcher;
-        };
-      };
-
-      apps = forAllSystems (pkgs:
-        let pkg = self.packages.${pkgs.system}.space-station-14-launcher; in {
-          default = self.apps.${pkgs.system}.space-station-14-launcher;
-          space-station-14-launcher = {
-            type = "app";
-            program = "${pkg}/bin/${pkg.meta.mainProgram}";
-          };
-          fetch-deps = {
-            type = "app";
-            program = toString
-              self.packages.${pkgs.system}.space-station-14-launcher.passthru.fetch-deps;
-          };
-        });
-
-      formatter = forAllSystems (pkgs: pkgs.nixpkgs-fmt);
+      formatter = forAllSystems (pkgs: pkgs.nixfmt-rfc-style);
     };
 }
--- shell.nix
+++ shell.nix
@@ -1,15 +1,19 @@
-{ pkgs ? (let lock = builtins.fromJSON (builtins.readFile ./flake.lock);
-in import (builtins.fetchTarball {
-  url =
-    "https://github.com/NixOS/nixpkgs/archive/${lock.nodes.nixpkgs.locked.rev}.tar.gz";
-  sha256 = lock.nodes.nixpkgs.locked.narHash;
-}) { }) }:
+{
+  pkgs ? (
+    let
+      lock = builtins.fromJSON (builtins.readFile ./flake.lock);
+    in
+    import (builtins.fetchTarball {
+      url = "https://github.com/NixOS/nixpkgs/archive/${lock.nodes.nixpkgs.locked.rev}.tar.gz";
+      sha256 = lock.nodes.nixpkgs.locked.narHash;
+    }) { }
+  ),
+}:
 
 let
   dependencies = with pkgs; [
     util-linux
     dotnetCorePackages.sdk_8_0
-    dotnetCorePackages.sdk_6_0
     glfw
     SDL2
     libGL
@@ -49,7 +53,8 @@ let
     cups
     fontconfig
   ];
-in pkgs.mkShell {
+in
+pkgs.mkShell {
   name = "space-station-14-devshell";
   buildInputs = [ pkgs.gtk3 ];
   packages = dependencies;
